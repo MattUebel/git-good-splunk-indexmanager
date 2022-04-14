381f9e5d650fa0c68b6320e886f6250227d8af48
MattUebel <mattuebel@github.com>
2022-04-14 16:55:50 -0400

    a script now

---
+++ .github/workflows/link_new_apps.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SPLUNK_ETC_DIR="/opt/splunk/etc/master-apps"
+SPLUNK_DEPLOYS_DIR="/opt/splunk_deploys"
+NEW_DEPLOY_DIR="$SPLUNK_DEPLOYS_DIR/$(date +%Y%m%d%H%M%S)"
+OLD_DIRECTORIES_TO_PRESERVER="5"
+
+# exit if SPLUNK_ETC_DIR is not a symlink
+if [ ! -L $SPLUNK_ETC_DIR ]; then
+  echo "SPLUNK_ETC_DIR is not a symlink. Exiting."
+  exit 1
+fi
+
+# create SPLUNK_DEPLOYS_DIR if it doesn't exist
+if [ ! -d "$SPLUNK_DEPLOYS_DIR" ]; then
+  mkdir -p "$SPLUNK_DEPLOYS_DIR"
+fi
+
+# create new deploy directory
+mkdir -p "$NEW_DEPLOY_DIR"
+
+# copy content of apps/ to new deploy directory
+cp -r apps/* $NEW_DEPLOY_DIR
+
+# symlink new deploy directory to SPLUNK_ETC_DIR
+ln -nsf $NEW_DEPLOY_DIR $SPLUNK_ETC_DIR
+
+# chown SPLUNK_DEPLOYS_DIR and SPLUNK_ETC_DIR
+chown -R splunk:splunk $SPLUNK_DEPLOYS_DIR
+chown -R splunk:splunk $SPLUNK_ETC_DIR
+
+# delete all but the last $OLD_DIRECTORIES_TO_PRESERVER directories in SPLUNK_DEPLOYS_DIR
+ls -t $SPLUNK_DEPLOYS_DIR | tail -n +$OLD_DIRECTORIES_TO_PRESERVER | xargs rm -rf
+
