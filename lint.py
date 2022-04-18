47cc80d39344cabb9350221d6ae3ffd7455cad77
MattUebel <mattuebel@github.com>
2022-04-18 18:31:18 -0400

    rework messaging

--- lint.py
+++ lint.py
@@ -4,7 +4,6 @@ import sys
 
 ALERT_THRESHOLD = 86400
 WARNING_THRESHOLD = 864000
-EXIT_CODE = 0
 ERROR_MESSAGES = []
 WARNING_MESSAGES = []
 OUTPUT_FILE = "output.txt"
@@ -24,23 +23,20 @@ for file in config_files:
             frozenTimePeriodInSecs = config.getint(section, "frozenTimePeriodInSecs")
             if frozenTimePeriodInSecs < ALERT_THRESHOLD:
                 ERROR_MESSAGES.append(
-                    f"`{file}` has a frozenTimePeriodInSecs of `{frozenTimePeriodInSecs}` which is less than the required value of `{ALERT_THRESHOLD}`"
+                    f"The index `{section}` in `{file}` has a frozenTimePeriodInSecs of `{frozenTimePeriodInSecs}` which is less than the required value of `{ALERT_THRESHOLD}`"
                 )
-                EXIT_CODE += 1
             elif frozenTimePeriodInSecs < WARNING_THRESHOLD:
                 WARNING_MESSAGES.append(
-                    f"`{file}` has a frozenTimePeriodInSecs of `{frozenTimePeriodInSecs}` which is less than the recommended value of `{WARNING_THRESHOLD}`"
+                    f"The index `{section}` in `{file}` has a frozenTimePeriodInSecs of `{frozenTimePeriodInSecs}` which is less than the recommended value of `{WARNING_THRESHOLD}`"
                 )
 
 with open(OUTPUT_FILE, "w") as f:
     if ERROR_MESSAGES:
         print(f"::set-output name=status::failure")
         f.write("### Errors :red_circle:\n")
-        for message in ERROR_MESSAGES:
-            f.write(message + "\n")
+        f.write("\n".join(ERROR_MESSAGES))
     else:
         f.write("### No errors found! :tada:\n")
     if WARNING_MESSAGES:
         f.write("\n### Warnings :warning:\n")
-        for message in WARNING_MESSAGES:
-            f.write(message + "\n")
+        f.write("\n".join(WARNING_MESSAGES))
