94ecf18391e6622aab6c1ee35bb9041dde6a34d2
MattUebel <mattuebel@github.com>
2022-04-18 11:02:27 -0400

    add exit codes to lint

--- lint.py
+++ lint.py
@@ -1,8 +1,12 @@
 import configparser
 import os
+import sys
 
 ALERT_THRESHOLD = 86400
 WARNING_THRESHOLD = 864000
+EXIT_CODE = 0
+ERROR_MESSAGES = []
+WARNING_MESSAGES = []
 
 # recursively find all .conf files in the app directory
 config_files = []
@@ -18,14 +22,22 @@ for file in config_files:
         if config.has_option(section, "frozenTimePeriodInSecs"):
             frozenTimePeriodInSecs = config.getint(section, "frozenTimePeriodInSecs")
             if frozenTimePeriodInSecs < ALERT_THRESHOLD:
-                print(
-                    "::error::{} has a frozenTimePeriodInSecs of {} which is less than the ALERT_THRESHOLD of {}".format(
-                        file, frozenTimePeriodInSecs, ALERT_THRESHOLD
-                    )
+                EXIT_MESSAGES.append(
+                    f"{file} has a frozenTimePeriodInSecs of {frozenTimePeriodInSecs} which is less than the required value of {ALERT_THRESHOLD}"
                 )
+                EXIT_CODE += 1
             elif frozenTimePeriodInSecs < WARNING_THRESHOLD:
-                print(
-                    "::warning::{} has a frozenTimePeriodInSecs of {} which is less than the WARNING_THRESHOLD of {}".format(
-                        file, frozenTimePeriodInSecs, WARNING_THRESHOLD
-                    )
+                WARNING_MESSAGES.append(
+                    f"{file} has a frozenTimePeriodInSecs of {frozenTimePeriodInSecs} which is less than the recommended value of {WARNING_THRESHOLD}"
                 )
+
+if EXIT_CODE > 0:
+    print(f"::error::{EXIT_CODE} errors found: {ERROR_MESSAGES}")
+    sys.exit(EXIT_CODE)
+else:
+    print("::success::No errors found")
+    if WARNING_MESSAGES:
+        print(
+            f"::warning::However, {len(WARNING_MESSAGES)} warnings found: {WARNING_MESSAGES}"
+        )
+    sys.exit(EXIT_CODE)
